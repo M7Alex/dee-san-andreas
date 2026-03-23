9c8d07f82458cb45834347d34e6c1967888688c2
M7Alex <b7alex07@gmail.com>
2026-03-23 22:41:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ignore TypeScript et ESLint errors pendant le build Vercel
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', '*.vercel.app'],
+      bodySizeLimit: '10mb',
     },
   },
   images: {
